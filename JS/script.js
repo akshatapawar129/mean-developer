e22bae7e951f19cb5c3d8fbec7089862f0b93ca7
Akshata Pawar <akshata.arjun.pawar@accenture.com>
2024-03-02 23:23:51 +0530

    updated files

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+function handleMouseOver(){
+ //   alert('Mouse Over detected');
+}
+
+//Variables
+var x = 10; //number
+var str = 'hello' //string
+isValid = false; //boolean
+
+var num1 = 10;
+var num2 = 20;
+
+//other datatype
+var person = {
+    name : 'sudha',
+    age : 28,
+}
+var z; //undefined
+function sum(n1, n2){
+    // n1 and n2 are called as function parameters  
+    n1 + n2;
+// alert(n1);
+}
+
+
+sum(10,20);// here 10 and 20 are called as arguments
+sum(30,44)
+
+// Operartors
+
+//1. Arithamatic Operators : + - * / ++ -- %
+n1+n2;
+n1-n2;
+n1*n2;
+n1/n2;
+n1%n2;
+n1++;
+n1--;
+
+//2. Assignment Operators
+x += y; // x = x + y
